57a716f506a9b95ed056e6c2aef9e0586d421bfa
Arianna Lee <ajlee338@gmail.com>
2020-02-25 21:36:09 -0500

    [Autonomous] Updated BlueLoadingOp due to changes in how we grab the skystones

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLoadingOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLoadingOp.java
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
@@ -37,8 +36,6 @@ public class BlueLoadingOp extends LinearOpMode {
     final double BASE_POWER_HORIZONTAL = 0.55;
 
     private final double ARM_GRABBER_MAX_POS = 0.55;
-    private final double SIDE_GRABBER_UP_POS = 0;
-    private final double CAPSTONE_MIN_POS = 0.33;
 
     private OdometryGlobalCoordinatePosition globalPositionUpdate;
 
@@ -89,9 +86,12 @@ public class BlueLoadingOp extends LinearOpMode {
     private DcMotor leftBack;
     private DistanceSensor distanceFront;
     private DistanceSensor distanceBack;
-    private Servo sideGrabber;
+    private DistanceSensor distanceRight;
+    private Servo frontSwing;
+    private Servo frontGrabber;
+    private Servo backSwing;
+    private Servo backGrabber;
     private Servo armGrabber;
-    private Servo capstone;
     private DistanceSensor distanceLeft;
     private Servo hookLeft;
     private Servo hookRight;
@@ -100,13 +100,14 @@ public class BlueLoadingOp extends LinearOpMode {
     public void runOpMode() {
         initDriveHardwareMap();
 
-        sideGrabber.setPosition(0);
-        hookLeft.setPosition(0);
-        hookRight.setPosition(0);
+        frontSwing.setPosition(0);
+        frontGrabber.setPosition(0);
+        backSwing.setPosition(0);
+        backGrabber.setPosition(0);
+        hookLeft.setPosition(1);
+        hookRight.setPosition(1);
 
         armGrabber.setPosition(ARM_GRABBER_MAX_POS);
-        sideGrabber.setPosition(SIDE_GRABBER_UP_POS);
-        capstone.setPosition(CAPSTONE_MIN_POS);
 
         initiateVuforia();
 
@@ -122,6 +123,9 @@ public class BlueLoadingOp extends LinearOpMode {
         if (opModeIsActive()) {
             // Activate Vuforia Skystone tracking
             targetsSkyStone.activate();
+            com.vuforia.CameraDevice.getInstance().setFlashTorchMode(true);
+            com.vuforia.CameraDevice.getInstance().setField("opt1-zoom", "opt1-zoom-on");
+            com.vuforia.CameraDevice.getInstance().setField("zoom", "30");
             //1. Move to about 10 inches in front of stone wall
             double distance = distanceLeft.getDistance(DistanceUnit.INCH) - 8;
             double targetX = -20.0;
@@ -129,7 +133,6 @@ public class BlueLoadingOp extends LinearOpMode {
                 targetX = distance * -1;
             }
 
-            navigator.goToPosition(targetX, 0, BASE_POWER_HORIZONTAL + 0.15, 0, 1);
             navigator.stop();
 
             //2. Call getSkystoneLocation
@@ -149,28 +152,27 @@ public class BlueLoadingOp extends LinearOpMode {
             double raw = 0.0;
 
             if (skystoneLocation == SkystoneLocation.FIRST) {
-                offset = Math.abs(yValue) / mmPerInch - 3.0;
+                offset = -2;
                 where = "FIRST";
-                raw = offset;
-                offset = Range.clip(raw, -1.0, 0);
-                yDistanceToNextStone = 23.0;
+                yDistanceToNextStone = 22.0;
                 telemetry.addData("Skystone Located", "FIRST");
-                nextYTarget = getFirstStoneRoutine(offset, yDistanceToNextStone);
+                nextYTarget = getFirstStoneRoutine(offset, frontSwing, frontGrabber, yDistanceToNextStone);
+                getNextStoneRoutine(nextYTarget, frontSwing, frontGrabber);
             } else if (skystoneLocation == SkystoneLocation.SECOND) {
-                offset = Math.abs(yValue) / mmPerInch + 4.0;
-                yDistanceToNextStone = 23.0;
+                offset = 0;
+                yDistanceToNextStone = 22.0;
                 telemetry.addData("Skystone Located", "SECOND");
                 where = "SECOND";
-                raw = offset;
-                offset = Range.clip(raw, 7.0, 9.0);
-                nextYTarget = getFirstStoneRoutine(offset, yDistanceToNextStone);
+                nextYTarget = getFirstStoneRoutine(offset, backSwing, backGrabber, yDistanceToNextStone);
+                getNextStoneRoutine(nextYTarget, backSwing, backGrabber);
             } else {
-                offset = 15.0;
-                yDistanceToNextStone = 8.0;
+                offset = -6.0;
+                yDistanceToNextStone = 22.0;
                 telemetry.addData("Skystone Located", "THIRD");
                 where = "THIRD";
                 raw = offset;
-                nextYTarget = getFirstStoneRoutine(offset, yDistanceToNextStone);
+                nextYTarget = getFirstStoneRoutine(offset, backSwing, backGrabber, yDistanceToNextStone);
+                getNextStoneRoutine(nextYTarget, backSwing, backGrabber);
             }
 
             telemetry.addData("Skystone", where);
@@ -178,7 +180,6 @@ public class BlueLoadingOp extends LinearOpMode {
             telemetry.addData("CLIP OFFSET", offset);
             telemetry.addData("Distance to wall", distanceBack.getDistance(DistanceUnit.INCH));
             telemetry.update();
-            getNextStoneRoutine(nextYTarget);
 
             while (opModeIsActive() && !isStopRequested()) {
                 sleep(50);
@@ -193,35 +194,51 @@ public class BlueLoadingOp extends LinearOpMode {
         }
     }
 
-    private void grabAndMoveStone() {
-        sideGrabber.setPosition(1);
-        sleep(500);
-        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH + 1, BASE_POWER_VERTICAL, 0, 0.5);
-        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH + 8, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH, BASE_POWER_HORIZONTAL, 0, 1);
+    private void grabAndMoveStone(Servo swing, Servo grabber) {
+        //Move under the bridge
+        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH + 5, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH, BASE_POWER_HORIZONTAL, 0, 1);
         navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH, 36, BASE_POWER_VERTICAL, 0, 1);
-        sideGrabber.setPosition(0);
+        //Release
+        swing.setPosition(0.8);
+        grabber.setPosition(1);
         sleep(500);
+        swing.setPosition(0.2);
+        grabber.setPosition(0);
     }
 
-    private double getFirstStoneRoutine(double offset, double yDistanceToNextStone) {
+    private double getFirstStoneRoutine(double offset, Servo swing, Servo grabber, double yDistanceToNextStone) {
         // Move slowly
-        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH - offset, 0.5, 0, 1);
-        double distance = measureDistanceToTarget();
+        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH + offset, 0.5, 0, 1);
+        swing.setPosition(0.6);
+        grabber.setPosition(1);
+        navigator.goToPosition(-23, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH, BASE_POWER_HORIZONTAL + 0.15, 0, 1);
+        //Grab the first stone
+        swing.setPosition(0.8);
+        grabber.setPosition(0.4);
+        sleep(500);
+        swing.setPosition(0.2);
+        com.vuforia.CameraDevice.getInstance().setFlashTorchMode(false);
+
         double nextYTarget = globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH - yDistanceToNextStone;
-        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH - distance, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH, 0.5, 0, 1);
-        grabAndMoveStone();
+
+        grabAndMoveStone(swing, grabber);
         return nextYTarget;
     }
 
-    private void getNextStoneRoutine(double targetY) {
+    private void getNextStoneRoutine(double targetY, Servo swing, Servo grabber) {
         navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH, targetY, BASE_POWER_VERTICAL, 0, 1.5);
+        navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH + 6, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH, BASE_POWER_HORIZONTAL, 0, 1);
         navigator.stop();
-
+        swing.setPosition(0.6);
+        grabber.setPosition(1);
         double distance = measureDistanceToTarget();
         navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH - distance, globalPositionUpdate.returnYCoordinate() / COUNTS_PER_INCH, 0.5, 0, 1);
         navigator.stop();
-
-        grabAndMoveStone();
+        swing.setPosition(0.8);
+        grabber.setPosition(0.4);
+        sleep(500);
+        swing.setPosition(0.2);
+        grabAndMoveStone(swing, grabber);
 
         navigator.goToPosition(globalPositionUpdate.returnXCoordinate() / COUNTS_PER_INCH - 2, 32, BASE_POWER_VERTICAL + 0.1, 0, 1);
     }
@@ -232,8 +249,10 @@ public class BlueLoadingOp extends LinearOpMode {
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         armGrabber = hardwareMap.servo.get("armGrabber");
-        sideGrabber = hardwareMap.servo.get("sideGrabber");
-        capstone = hardwareMap.servo.get("capstone");
+        frontSwing = hardwareMap.servo.get("frontSwing");
+        frontGrabber = hardwareMap.servo.get("frontGrabber");
+        backSwing = hardwareMap.servo.get("backSwing");
+        backGrabber = hardwareMap.servo.get("backGrabber");
         distanceFront = hardwareMap.get(DistanceSensor.class, "distanceFront");
         distanceLeft = hardwareMap.get(DistanceSensor.class, "distanceLeft");
         distanceBack = hardwareMap.get(DistanceSensor.class, "distanceBack");
@@ -249,8 +268,8 @@ public class BlueLoadingOp extends LinearOpMode {
 
     private void initiateVuforia() {
         // TODO - set 'cameraMonitorViewId' to zero to disable camera monitoring
-        //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(0);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraDirection = CAMERA_CHOICE;
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
@@ -292,11 +311,10 @@ public class BlueLoadingOp extends LinearOpMode {
     private double measureDistanceToTarget() {
         double distance = distanceLeft.getDistance(DistanceUnit.INCH) + 1;
         if (distance > 15) {
-            distance = 12.0;
+            distance = 15.0;
         }
         return distance;
     }
-
 
 
     private SkystoneLocation getSkystoneLocation(double offset) {
@@ -313,15 +331,11 @@ public class BlueLoadingOp extends LinearOpMode {
     private double getSkystoneYValue(List<VuforiaTrackable> allTrackables) {
         double yValue = 1000.0;
 
-        SkystoneLocation skystoneLocation = SkystoneLocation.THIRD;
-
-        ElapsedTime elapsedTime = new ElapsedTime();
         boolean targetVisible = false;
 
-        while (!isStopRequested() && !targetVisible && (elapsedTime.milliseconds() < 2000)) {
-
+        ElapsedTime elapsedTime = new ElapsedTime();
+        while (!isStopRequested() && !targetVisible && (elapsedTime.milliseconds() < 500)) {
             // check all the trackable targets to see which one (if any) is visible.
-            targetVisible = false;
             for (VuforiaTrackable trackable : allTrackables) {
                 if (((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) {
                     telemetry.addData("Visible Target", trackable.getName());
@@ -336,24 +350,24 @@ public class BlueLoadingOp extends LinearOpMode {
                     break;
                 }
             }
-
-            // Provide feedback as to where the robot is located (if we know).
-            if (targetVisible) {
-                // express position (translation) of robot in inches.
-                VectorF translation = lastLocation.getTranslation();
-                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
-
-                // express the rotation of the robot in degrees.
-                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
-
-                yValue = translation.get(1);
-            } else {
-                telemetry.addData("Visible Target", "none");
-            }
-            telemetry.update();
         }
+
+        // Provide feedback as to where the robot is located (if we know).
+        if (targetVisible) {
+            // express position (translation) of robot in inches.
+            VectorF translation = lastLocation.getTranslation();
+            telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                    translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+            // express the rotation of the robot in degrees.
+            Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+            telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+
+            yValue = translation.get(1);
+        } else {
+            telemetry.addData("Visible Target", "none");
+        }
+        telemetry.update();
         return yValue;
     }
 }
